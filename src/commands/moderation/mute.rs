aefb6583eb238644ad924136c7d78a42d04b5c55
Meetzli <63364127+meetzli@users.noreply.github.com>
2024-08-24 18:36:25 +0200

    chore(mute): rewrite for internationalisation

--- src/commands/moderation/mute.rs
+++ src/commands/moderation/mute.rs
@@ -6,6 +6,7 @@ use poise::serenity_prelude::{EditMember, Member};
 use diesel::prelude::*;
 use crate::{BotError, Context};
 use crate::database::models::Cases;
+use crate::localization::manager::TranslationParam;
 use crate::modules::moderation::logs::{log_action, LogData, LogType};
 use crate::util::time::{date_after, parse_to_time};
 
@@ -22,30 +23,73 @@ pub async fn mute(
 ) -> Result<(), BotError> {
     use crate::database::schema::cases::dsl::*;
     
+    let db = ctx.data().db.clone();
+    let locales = ctx.data().localization_manager.clone();
+    
+    let guild_lang = locales
+        .get_guild_language(db, ctx.guild_id().unwrap()).await.unwrap();
+    
+    let guild = ctx.guild().unwrap().clone();
+    
     if user.user.bot() {
-        send_reply(ctx, CreateReply::new().content("You can't mute a bot").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_user_bot", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
         return Ok(());
     }
 
     if user.user.id == ctx.author().id {
-        send_reply(ctx, CreateReply::new().content("You can't mute yourself").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_user_self", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
+        return Ok(());
+    }
+    
+    if user.user.id == ctx.guild().unwrap().owner_id {
+        let error_msg = locales.get("commands.moderation.mute.error_user_owner", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
         return Ok(());
     }
 
     if user.permissions(ctx.cache()).unwrap().administrator() {
-        send_reply(ctx, CreateReply::new().content("You can't mute an administrator").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_user_admin", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
+        return Ok(());
+    }
+
+    let author_highest_role_position = guild.member_highest_role(&ctx.author_member().await.unwrap()).map(|r| r.position).unwrap_or(0);
+
+    let user_highest_role_position = guild.member_highest_role(&user).map(|r| r.position).unwrap_or(0);
+
+    let bot_highest_role_position = guild.member_highest_role(&guild.id.member(ctx.http(), ctx.http().get_current_user().await.unwrap().id).await.unwrap()).map(|r| r.position).unwrap_or(0);
+
+    if user_highest_role_position >= bot_highest_role_position {
+        let error_msg = locales.get("commands.moderation.mute.error_user_higher_role", guild_lang, &[]).await;
+
+        send_reply(ctx,
+                   CreateReply::new().content(error_msg).ephemeral(true)
+        ).await?;
+        return Ok(());
+    }
+
+    if guild.owner_id != ctx.author().id && author_highest_role_position <= user_highest_role_position {
+        let error_msg = locales.get("commands.moderation.mute.error_user_higher_role", guild_lang, &[]).await;
+
+        send_reply(ctx,
+                   CreateReply::new().content(error_msg).ephemeral(true)
+        ).await?;
         return Ok(());
     }
 
     if parse_to_time(duration.clone()).is_none() {
-        send_reply(ctx, CreateReply::new().content("Invalid time format").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_invalid_time", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
         return Ok(());
     }
 
     let date = date_after(parse_to_time(duration.clone()).unwrap());
 
     if date.timestamp() - Utc::now().timestamp() > 2419200 {
-        send_reply(ctx, CreateReply::new().content("You can't mute a user for more than 28 days").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_time_too_long", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
         return Ok(());
     }
 
@@ -59,7 +103,8 @@ pub async fn mute(
     }).await.ok().is_some();
 
     if  is_muted {
-        send_reply(ctx, CreateReply::new().content("User is already muted").ephemeral(true)).await?;
+        let error_msg = locales.get("commands.moderation.mute.error_user_already_muted", guild_lang, &[]).await;
+        send_reply(ctx, CreateReply::new().content(error_msg).ephemeral(true)).await?;
         return Ok(());
     }
 
@@ -103,10 +148,15 @@ pub async fn mute(
             .execute(conn)
     }).await?;
 
-    send_reply(ctx, CreateReply::new().content(format!(
-        "Muted {} for {} with reason: {}",
-        user.user.tag(), duration, action_reason.as_deref().unwrap_or("No reason provided"))
-    ).ephemeral(false)).await?;
+    let no_reason = locales.get("commands.moderation.mute.no_reason", guild_lang, &[]).await;
+    
+    let content = locales.get("commands.moderation.mute.reply_success", guild_lang, &[
+        TranslationParam::String(user.user.tag()),
+        TranslationParam::String(duration.clone()),
+        TranslationParam::String(action_reason.as_deref().unwrap_or(&*no_reason).to_string())
+    ]).await;
+    
+    send_reply(ctx, CreateReply::new().content(content).ephemeral(false)).await?;
 
     let data = ctx.data().clone();
 
